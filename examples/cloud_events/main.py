209a8d66ebf418aeeb21b7e030073177b4f26d29
joelgerard <joelgerard@gmail.com>
2020-06-16 14:54:51 -0700

    A sample that shows how to use a CloudEvent.

---
+++ examples/cloud_events/main.py
@@ -0,0 +1,22 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This sample creates a function that accepts a Cloud Event per
+# https://github.com/cloudevents/sdk-python
+import sys
+
+def hello(cloud_event):
+    print("Received event with ID: %s" % cloud_event.EventID(), file=sys.stdout, flush=True)
+    return 200
+
