b2c2182cea44880c1e6d3f96d524abb7362b2a54
joelgerard <joelgerard@gmail.com>
2020-06-18 14:06:08 -0700

    Convert shell to docker py api

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -1,33 +1,34 @@
-import pytest
-import os
-import sh
+import pathlib
 import time
+
 import docker
+import pytest
+import requests
+
+SAMPLES_DIR = pathlib.Path(__file__).resolve().parent.parent / "examples"
 
 
 @pytest.mark.slow_integration_test
 def test_cloud_run_http():
     client = docker.from_env()
-
-    cwd = os.getcwd()
-    os.chdir(cwd + "/../examples/cloud_run_http")
     TAG = "cloud_run_http"
-    sh.docker(["build", "--no-cache", "--tag=%s" % TAG, "."])
-    cmd = sh.docker(["run", "-p:8080:8080", "-d", TAG])
-    id = cmd.stdout.decode("utf-8")
 
-    container_up = False
+    client.images.build(path=str(SAMPLES_DIR / "cloud_run_http"), tag={TAG})
+    # TODO(joelgerard): Check port and deflake, etc.
+    container = client.containers.run(image=TAG, detach=True, ports={8080: 8080})
     timeout = 10
-    while not container_up:
-        output = sh.docker(["ps", "-f id=%s" % id, "-f status=running", "-q"]).stdout.decode("utf-8")
-        container_up = output != ""
+    success = False
+    while success == False and timeout > 0:
+        try:
+            response = requests.get("http://localhost:8080")
+            if response.text == "Hello world!":
+                success = True
+        except:
+            pass
+
         time.sleep(1)
         timeout -= 1
-        if timeout == 0:
-          assert 0
 
-    sample_output = sh.curl("http://localhost:8080")
-    assert sample_output == "Hello world!"
+    container.stop()
 
-    sh.docker(["stop", id])
-
+    assert success
