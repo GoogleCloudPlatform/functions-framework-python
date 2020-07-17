4cbb772159d9d55410acd817b774150ebd12e696
joelgerard <joelgerard@gmail.com>
2020-07-17 09:17:09 -0700

    Fix up CE json

--- tests/conformance_tests/cloud_event_test.py
+++ tests/conformance_tests/cloud_event_test.py
@@ -1,10 +1,7 @@
-from cloudevents.sdk import converters
-from cloudevents.sdk import marshaller
-from cloudevents.sdk.converters import structured
+import json
+
 
 def test(cloudevent):
-    m = marshaller.NewHTTPMarshaller([structured.NewJSONHTTPCloudEventConverter()])
-    headers, body = m.ToRequest(cloudevent, converters.TypeStructured, lambda x: x)
     text_file = open("function_output.json", "w")
-    text_file.write(body.getvalue().decode("utf-8"))
+    text_file.write(json.dumps(cloudevent.Data()))
     text_file.close()
