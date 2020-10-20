ccb964cb8bb17d024c5c7e008dfa32b4d183ea01
Dustin Ingram <di@users.noreply.github.com>
2020-10-20 14:06:19 -0500

    Support Python 3.9

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords="functions-framework",
     packages=find_packages(where="src"),
