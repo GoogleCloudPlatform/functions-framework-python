365eeccbe3f6ebdba50f57ba661a1274fad00d4a
Dustin Ingram <di@users.noreply.github.com>
2020-10-20 14:30:26 -0500

    Add conformance tests

---
+++ tests/conformance/main.py
@@ -0,0 +1,32 @@
+import json
+
+filename = "function_output.json"
+
+
+def _write_json(content):
+    with open(filename, "w") as f:
+        f.write(json.dumps(content))
+
+
+def write_http(request):
+    _write_json(request.json)
+    return "OK", 200
+
+
+def write_legacy_event(data, context):
+    _write_json(
+        {
+            "data": data,
+            "context": {
+                "eventId": context.eventId,
+                "timestamp": context.timestamp,
+                "eventType": context.eventType,
+                "resource": context.resource,
+            },
+        }
+    )
+
+
+def write_cloud_event(cloudevent):
+    cloudevent.datacontenttype = "application/json"
+    _write_json(cloudevent)
