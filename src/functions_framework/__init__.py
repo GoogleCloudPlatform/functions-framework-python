9fbc2f6f8b39a87abac0d119b27daf5fe2d39894
Matthew Robertson <mattrobertson@google.com>
2021-04-30 13:05:04 -0700

    convert cloud events to background events (#124)
    * convert cloud events to background events
    
    * review feedback

--- src/functions_framework/__init__.py
+++ src/functions_framework/__init__.py
@@ -112,7 +112,11 @@ def _cloudevent_view_func_wrapper(function, request):
 
 def _event_view_func_wrapper(function, request):
     def view_func(path):
-        if is_binary(request.headers):
+        if event_conversion.is_convertable_cloudevent(request):
+            # Convert this CloudEvent to the equivalent background event data and context.
+            data, context = event_conversion.cloudevent_to_background_event(request)
+            function(data, context)
+        elif is_binary(request.headers):
             # Support CloudEvents in binary content mode, with data being the
             # whole request body and context attributes retrieved from request
             # headers.
