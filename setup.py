95fe10c6130bd9553a4b3829f2d62d7e2802160f
Dustin Ingram <di@users.noreply.github.com>
2021-05-17 16:20:07 -0400

    Relax constraint to flask<3.0 and click<9.0

--- setup.py
+++ setup.py
@@ -49,8 +49,8 @@ setup(
     package_dir={"": "src"},
     python_requires=">=3.5, <4",
     install_requires=[
-        "flask>=1.0,<2.0",
-        "click>=7.0,<8.0",
+        "flask>=1.0,<3.0",
+        "click>=7.0,<9.0",
         "watchdog>=1.0.0,<2.0.0",
         "gunicorn>=19.2.0,<21.0; platform_system!='Windows'",
         "cloudevents>=1.2.0,<2.0.0",
