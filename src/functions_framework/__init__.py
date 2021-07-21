9ea8dd2d7ccc88db6ab2c023859f74e7ead7f7f7
Dustin Ingram <di@users.noreply.github.com>
2021-07-21 11:34:21 -0500

    fix: Add a DummyErrorHandler (#137)
    * Remove unused imports
    
    * Add failing test
    
    * Add DummyErrorHandler

--- src/functions_framework/__init__.py
+++ src/functions_framework/__init__.py
@@ -348,3 +348,14 @@ class LazyWSGIApp:
 
 
 app = LazyWSGIApp()
+
+
+class DummyErrorHandler:
+    def __init__(self):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return self
+
+
+errorhandler = DummyErrorHandler()
