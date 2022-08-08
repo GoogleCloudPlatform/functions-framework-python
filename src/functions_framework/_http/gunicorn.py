3ec64fd50789c1f7f1fb799ef714e7cb4b559a22
Pratiksha Kap <pratikshakap@google.com>
2022-08-08 14:22:52 -0700

    remove unused imports

--- src/functions_framework/_http/gunicorn.py
+++ src/functions_framework/_http/gunicorn.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import gunicorn.app.base
-import os
+import gunicorn.app.BaseApplication
+
 
 class GunicornApplication(gunicorn.app.base.BaseApplication):
     def __init__(self, app, host, port, debug, **options):
