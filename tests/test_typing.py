977cf967ac5bf1f683a83ff8ea358f771ccb5454
Jonathan Ballet <jon@multani.info>
2023-08-28 22:28:35 +0200

    fix import order

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -2,8 +2,10 @@ import typing
 
 if typing.TYPE_CHECKING:  # pragma: no cover
     import flask
-    import functions_framework
+
     from cloudevents.http.event import CloudEvent
+
+    import functions_framework
 
     @functions_framework.http
     def hello(request: flask.Request) -> flask.typing.ResponseReturnValue:
