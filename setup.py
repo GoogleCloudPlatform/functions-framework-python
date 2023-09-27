9c72c825337ad2da4c53c9d9edf712260609d7ad
HKWinterhalter <HansWinterhalter@gmail.com>
2023-09-27 14:57:54 -0700

    chore(deps): Remove gunicorn version upper bound

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
         "flask>=1.0,<3.0",
         "click>=7.0,<9.0",
         "watchdog>=1.0.0",
-        "gunicorn>=19.2.0,<21.0; platform_system!='Windows'",
+        "gunicorn>=19.2.0; platform_system!='Windows'",
         "cloudevents>=1.2.0,<2.0.0",
     ],
     entry_points={
