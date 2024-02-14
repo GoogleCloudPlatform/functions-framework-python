3d2095cd8d0814b44bc2351ea02d39febdba9314
June <121135073+KAKAinGoog@users.noreply.github.com>
2024-02-14 13:42:43 -0800

    fix!: avoid long running process when request timeout (#309)
    Previously function framework use 0 timeout which is actually "no timeout" restrction.
     This was causing a problem that when user provides a request timeout to Cloud function, process will still continue and consume resources.
     In this fix, timeout is enabled; default timeout settings is 5 min, same as Cloud run.
     To make sure timeout settings will be respected, default settings switched from multi-threads to multi-workers.
    
     However, user is still allowed to  customize workers/threads by assigning env var. But user need to note that timeout won't work when #thread > 1.

--- tests/test_http.py
+++ tests/test_http.py
@@ -97,17 +97,17 @@ def test_gunicorn_application(debug):
     assert gunicorn_app.app == app
     assert gunicorn_app.options == {
         "bind": "%s:%s" % (host, port),
-        "workers": 1,
-        "threads": os.cpu_count() * 4,
-        "timeout": 0,
+        "workers": os.cpu_count() * 4,
+        "threads": 1,
+        "timeout": 300,
         "loglevel": "error",
         "limit_request_line": 0,
     }
 
     assert gunicorn_app.cfg.bind == ["1.2.3.4:1234"]
-    assert gunicorn_app.cfg.workers == 1
-    assert gunicorn_app.cfg.threads == os.cpu_count() * 4
-    assert gunicorn_app.cfg.timeout == 0
+    assert gunicorn_app.cfg.workers == os.cpu_count() * 4
+    assert gunicorn_app.cfg.threads == 1
+    assert gunicorn_app.cfg.timeout == 300
     assert gunicorn_app.load() == app
 
 
